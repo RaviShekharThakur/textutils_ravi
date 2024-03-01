23241805bb78a672fd1213be634c66176db76544
Mayank <mayank.jain.ror@gmail.com>
2024-03-01 11:32:50 +0530

    project initial commit

---
+++ src/components/About.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <>
+            <div>
+                <div id="accordion-collapse" data-accordion="collapse">
+                    <h2 id="accordion-collapse-heading-1">
+                        <button type="button" className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+                        <span>What is Flowbite?</span>
+                        <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5"/>
+                        </svg>
+                        </button>
+                    </h2>
+                    <div id="accordion-collapse-body-1" className="hidden" aria-labelledby="accordion-collapse-heading-1">
+                        <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+                        <p className="mb-2 text-gray-500 dark:text-gray-400">Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button type="button" class="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">change to dark mode</button>
+        </>
+    )
+}
---
+++ src/components/Textform.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react'
+
+export default function Textform(props) {
+  const handleUpClick = () =>{
+    let newText = text.toUpperCase();
+    setText(newText)
+  }
+
+  const handledownClick = () =>{
+    let newText = text.toLowerCase();
+    setText(newText)
+  }
+  const toTitleCase = (str) => {
+    return str.replace(/\w\S*/g, (txt) => {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+  };
+
+  const handleTitleClick = () => {
+    let newText = toTitleCase(text);
+    setText(newText);
+  };
+
+  const handleOnChange = (event) => {
+    console.log("onchanege")
+    setText(event.target.value)
+  }
+  const[text, setText] = useState('enter text here')
+  return (
+    <>
+      <div className='container'>
+        <form className="max-w-sm mx-auto">
+            <div className="mb-5">
+                <label htmlFor="myBox" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{props.heading}</label>
+                <input type="text" id="myBox" className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={handleOnChange} value={text}/>
+                <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" onClick={handleUpClick}>Convert to uppercase</button>
+                <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" onClick={handledownClick}>Convert to Lowercase</button>
+                <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" onClick={handleTitleClick}>Convert to titlecase</button>
+            </div>
+        </form>
+      </div>
+      <div className='container'>
+        <div className='max-w-sm mx-auto'>
+          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-4xl dark:text-white">your text summary</h1>
+          <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-4 dark:text-gray-400">{text.split(" ").length} and {text.length}</p>       
+          <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-4 dark:text-gray-400">{0.008 * text.split(" ").length} minutes read</p>      
+        </div>
+      </div>
+   </>
+  )
+}
